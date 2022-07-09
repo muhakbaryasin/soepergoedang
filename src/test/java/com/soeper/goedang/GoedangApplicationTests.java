462c7de4bba7de8de64cf40da6173dc8131cecea
Akbar Yasin <akbar@companieshouse.id>
2022-07-09 14:17:14 +0800

    Init

---
+++ src/test/java/com/soeper/goedang/GoedangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soeper.goedang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoedangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
